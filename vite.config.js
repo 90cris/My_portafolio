45b813d0d2e4ff2b969dfe9294578845970573ba
90cris <166570889+90cris@users.noreply.github.com>
2025-04-10 17:13:35 -0400

    fix: fallback para rutas en Render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // importante si el dominio es personalizado
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    // no afecta en producción pero útil para dev local
+    open: true,
+  }
 })
